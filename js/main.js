8bcde358a6030ab0913b9f9b46fd7a3c0574ccb8
sanjarbekdavlatov1996 <sanjarbekdavlatov.1996@gmail.com>
2022-11-19 23:14:01 +0500

    just do it

---
+++ js/main.js
@@ -0,0 +1,33 @@
+"use strict"
+//13 shartli operatorlar
+
+
+
+// const age = +prompt("How old are you","");
+
+// if (age >= 25) {
+//     console.log("Horror films");
+// } else if(age > 18 ){
+//     console.log("Boyavik films");
+// }else{
+//     console.log("Cartoon");
+// }
+
+// age > 25 ? console.log("Horror film") :console.log("cartoon");
+
+const color = prompt("","");
+
+switch(color){
+    case "red":
+        console.log("Stop");
+        break;
+    case "green":
+        console.log("Goo");
+        break;
+    case "yellow":
+        console.log("Slowly");
+        break;
+    default:
+        console.log("!Error ");
+        break;
+}
